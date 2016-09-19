e58b2be98f71a9147ce4f46f30cbd83c8792e24f
kzhembrovska <kzhembrovska@cca.edu>
2016-09-19 10:20:36 -0700

    Create inClass3.js

---
+++ inClass3.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(200,200);
+  background(255);
+}
+
+function draw() {
+  noFill();
+  stroke (10);
+  rect (20, 30, 60, 60);
+  rect (60, 70, 10, 20);
+  line (20, 30, 50, 0);
+  line (50, 0, 80, 30);
+  }
