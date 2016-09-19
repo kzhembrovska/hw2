57a74e78c0092d062b17a126854a712843db9255
kzhembrovska <kzhembrovska@cca.edu>
2016-09-19 09:50:05 -0700

    Create inClass1.js

---
+++ inClass1.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(200, 200);
+  background(255);
+} 
+
+function draw() {
+  fill(255);
+  stroke(0);
+  line(10,10,10,-10);
+  line(40, 10, 40, -9);
+  line(70, 10, 70, -8);
+  line(100, 10, 100, -7);
+  line(130, 10, 130, -6);
+  line(160, 10, 160, -5);
+  line(190, 10, 190, -4);
+  line(220, 10, 220, -3);
+}
