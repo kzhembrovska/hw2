9acf8a040a3c4dede1b1369c080a2caf29513509
kzhembrovska <kzhembrovska@cca.edu>
2016-09-24 01:23:02 -0700

    Update Hw2.js

--- Hw2.js
+++ Hw2.js
@@ -1,1 +1,15 @@
+function setup() {
+  createCanvas(200, 200);
+  background(0);
+}
 
+function draw() {
+   
+  for(i = 0; i <= 190; i += 10) {
+       for (j = 0; j <= 190; j -= 10) {
+    
+       ellipse (i+10, j-10, 5, 5);
+       fill(255);
+    }
+  }
+}
