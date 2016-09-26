f159723370f0da5dd7dffd996930905fd00588f0
kzhembrovska <kzhembrovska@cca.edu>
2016-09-26 10:44:09 -0700

    Update hw4.js

--- hw4.js
+++ hw4.js
@@ -2,13 +2,13 @@ function SetUp(){
   createCanvas(200,200);
   
   colorMode(HSB);
-  var h=16;
+  var h=0;
   noStroke();
   
   for (var y=20; y<=180; y=y+10){
    for (var x=20; x<=180; x=x+10){
     fill(h, 100, 100);
-    h=h+1;
+    h=h+1.3;
     ellipse(x,y,10,10);
     }
   }
