2c11fe47edf0fc28f11742a6fe13672d375df8f4
kzhembrovska <kzhembrovska@cca.edu>
2016-09-26 10:37:40 -0700

    Create hw4.js

---
+++ hw4.js
@@ -0,0 +1,19 @@
+function SetUp(){
+  createCanvas(200,200);
+  
+  colorMode(HSB);
+  var h=16;
+  noStroke();
+  
+  for (var y=20; y<=180; y=y+10){
+   for (var x=20; x<=180; x=x+10){
+    fill(h, 100, 100);
+    h=h+1;
+    ellipse(x,y,10,10);
+    }
+  }
+}
+
+function draw(){
+  
+}
