6b16c72fe6900b792580773ae858ae0a447a0cb4
kzhembrovska <kzhembrovska@cca.edu>
2016-09-26 10:25:02 -0700

    Create Hw5.js

---
+++ Hw5.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(210, 210);
+  background(0);
+
+stroke(255);
+  for(var i = 10; i <=200 ; i = i +5){
+    var startX = 10;
+    var startY = i;
+    var endX = i;
+    var endY = 10;
+    line(startX, startY, endX, endY);
+  }
+  
+  for(var i=15; i <=200; i=i+5){
+    var startX = 200;
+    var startY = i;
+    var endX = i;
+    var endY = 200;
+    line(startX, startY, endX, endY);
+  }
+  
+  textSize(200);
+  text("P", 50, 180)
+}
