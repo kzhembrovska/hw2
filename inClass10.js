2aeb846e14a5323a40aa35e222656e83cd587a36
kzhembrovska <kzhembrovska@cca.edu>
2016-09-26 09:03:58 -0700

    Create inClass10.js

---
+++ inClass10.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(200, 200);
+  background(255);
+  noStroke();
+}
+
+function draw() {
+  for(i = 10; i <= 190; i = i+5) {
+    for (j = 10; j <= 190; j=j+5) {
+      var a=random(255);
+      var b=random(255);
+       var c=random(255);
+       fill(a, b, c);
+       ellipseMode(CENTER);
+       ellipse (i, j, 5, 5);
+       fill(255);
+    }
+  }
+}
